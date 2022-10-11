1d61a9ec99d1fdb8849d0d298d00a971c11230bc
AnsariSaud <93068269+AnsariSaud@users.noreply.github.com>
2022-10-11 10:36:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,19 +34,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        f = request.files['file']
 
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-        value=getResult(file_path)
-        result=get_className(value) 
-        return result
-    return None
 
 
 if __name__ == '__main__':
